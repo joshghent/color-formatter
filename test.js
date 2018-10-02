57ef2867aaa2d9dba1b6ef6f85ce0968bca7025d
Prakhar0409 <prakhar0409@gmail.com>
2018-10-02 16:32:50 +0530

    Updated fork

--- test.js
+++ test.js
@@ -4,4 +4,3 @@ const assert = require('assert');
 // assert.deepEqual(Color.toRGB('#F8E100'), '248, 225, 0');
 
 assert.deepEqual(Color.toHex('100,200,255'), '#64C8FF');
-console.log(Color.toRgb('#64C8FF'));
