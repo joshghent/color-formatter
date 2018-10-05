0b99f5564f939b4e665cca2cf23ecbb014186e01
SG5 <sergeyv89@gmail.com>
2018-10-05 13:59:43 +0300

    add test for color as array

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const Color = require('./index.js');
 const assert = require('assert');
 
-// assert.deepEqual(Color.toRGB('#F8E100'), '248, 225, 0');
+assert.deepEqual(Color.toRgb('#F8E100'), '248,225,0');
+assert.deepEqual(Color.toRgb('#FADE00', true), [250,222,0]);
 
 assert.deepEqual(Color.toHex('100,200,255'), '#64C8FF');
