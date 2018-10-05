9caf7a83b9894d85f0cbe644ab72b5e18e58998d
SG5 <sergeyv89@gmail.com>
2018-10-05 14:33:56 +0300

    add tests

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ var colorObj = {
   toRgb: function (hexStr) {
     var nums = [];
     if (hexStr.length === 4) {
-      nums.push(parseInt(hexStr.charAt(1), 16));
-      nums.push(parseInt(hexStr.charAt(2), 16));
-      nums.push(parseInt(hexStr.charAt(3), 16));
+      nums.push(parseInt(hexStr.charAt(1) + hexStr.charAt(1), 16));
+      nums.push(parseInt(hexStr.charAt(2) + hexStr.charAt(2), 16));
+      nums.push(parseInt(hexStr.charAt(3) + hexStr.charAt(3), 16));
     } else if (hexStr.length === 7) {
       nums.push(parseInt(hexStr.charAt(1) + hexStr.charAt(2), 16));
       nums.push(parseInt(hexStr.charAt(3) + hexStr.charAt(4), 16));
