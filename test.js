3667a6ce924e0e44b5d4a4aa4dba5f68cea0fa75
Antoine Amara <amara.antoine@gmail.com>
2018-10-09 23:35:40 +0200

    test: add mocha and nyc to run unit tests and display coverage.
    Add mocha and nyc dependencies to package.json.
    Adapt unit test for mocha, for that add describe and it functions calls to
    construct mocha tests.
    Update the npm command to run nyc and mocha.
    
    Fixes: https://github.com/joshghent/color-formatter/issues/22

--- test.js
+++ test.js
@@ -1,28 +1,50 @@
-const Color = require('./index.js');
-const assert = require('assert');
+const Color = require("./index.js");
+const assert = require("assert");
 
-// assert.deepEqual(Color.toRGB('#F8E100'), '248, 225, 0');
+describe("Color Formatter", () => {
+    describe("convert RGB color to HEX color", () => {
+        it("should convert color rgb(100, 200, 255) to #64C8FF", () => {
+            assert.deepEqual(Color.toHex("100,200,255"), "#64C8FF");
+        });
+    });
 
-assert.deepEqual(Color.toHex('100,200,255'), '#64C8FF');
+    /*describe("convert HEX color to RGB color", () => {
+        it("should convert #F8E100 to rgb(248, 225, 0)", () => {
+            assert.deepEqual(Color.toRGB("#F8E100"), "248, 225, 0");
+        });
+    });*/
 
-// getType
-assert.deepEqual(Color.getType('#000000'), 'hex');
-assert.deepEqual(Color.getType('#000'), 'hex');
-assert.deepEqual(Color.getType('#ABC'), 'hex');
-assert.deepEqual(Color.getType('#abc'), 'hex');
-assert.deepEqual(Color.getType('#aBc'), 'hex');
-assert.deepEqual(Color.getType('#A01'), 'hex');
-assert.deepEqual(Color.getType('0, 0, 0'), 'rgb');
-assert.deepEqual(Color.getType('255, 255, 255'), 'rgb');
-assert.deepEqual(Color.getType('123, 123, 123'), 'rgb');
-assert.deepEqual(Color.getType('000000'), undefined);
-assert.deepEqual(Color.getType('#1234'), undefined);
-assert.deepEqual(Color.getType('#12345'), undefined);
-assert.deepEqual(Color.getType('#12345X'), undefined);
-assert.deepEqual(Color.getType('0, 0, 256'), undefined);
-assert.deepEqual(Color.getType('0, 0, 01'), undefined);
-assert.deepEqual(Color.getType('255, 255, 0255'), undefined);
-assert.deepEqual(Color.getType('10, 10, 1000'), undefined);
-assert.deepEqual(Color.getType('1,1,1'), undefined);
-assert.deepEqual(Color.getType(' 1, 1, 1'), undefined);
-assert.deepEqual(Color.getType('1, 1, 1 '), undefined);
+    describe("autodetect color type and return it", () => {
+      it("should detect hex types if this one is a valid color", () => {
+        assert.deepEqual(Color.getType("#000000"), "hex");
+        assert.deepEqual(Color.getType("#000"), "hex");
+        assert.deepEqual(Color.getType("#ABC"), "hex");
+        assert.deepEqual(Color.getType("#abc"), "hex");
+        assert.deepEqual(Color.getType("#aBc"), "hex");
+        assert.deepEqual(Color.getType("#A01"), "hex");
+      });
+
+      it("should return undefined if the HEX color is invalid", () => {
+        assert.deepEqual(typeof Color.getType("000000"), "undefined");
+        assert.deepEqual(typeof Color.getType("#1234"), "undefined");
+        assert.deepEqual(typeof Color.getType("#12345"), "undefined");
+        assert.deepEqual(typeof Color.getType("#12345X"), "undefined");
+      });
+
+      it("should detect the RGB type and return it if this one is a valid color", () => {
+        assert.deepEqual(Color.getType("0, 0, 0"), "rgb");
+        assert.deepEqual(Color.getType("255, 255, 255"), "rgb");
+        assert.deepEqual(Color.getType("123, 123, 123"), "rgb");
+      });
+
+      it("should return undefined if the RGB color is invalid", () => {
+        assert.deepEqual(typeof Color.getType("0, 0, 256"), "undefined");
+        assert.deepEqual(typeof Color.getType("0, 0, 01"), "undefined");
+        assert.deepEqual(typeof Color.getType("255, 255, 0255"), "undefined");
+        assert.deepEqual(typeof Color.getType("10, 10, 1000"), "undefined");
+        assert.deepEqual(typeof Color.getType("1,1,1"), "undefined");
+        assert.deepEqual(typeof Color.getType(" 1, 1, 1"), "undefined");
+        assert.deepEqual(typeof Color.getType("1, 1, 1 "), "undefined");
+      });
+    });
+});
