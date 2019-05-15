df310507326e607fc45cbe0e81cebebc2e7cfd1f
Josh Ghent <josh.ghent@cloudcall.com>
2019-05-15 10:22:34 +0100

    Fixed up tests

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 declare namespace Color {
     function toHex(str: string): string;
-    function toRgb(hexStr: string): string;
+    function toRgb(hexStr: string, asArray?: boolean): string | string[];
+    function getType(colorStr: string): string | undefined;
 }
 
 export = Color;
--- test.js
+++ test.js
@@ -3,59 +3,59 @@ const assert = require("assert");
 
 
 describe("Color Formatter", () => {
-    describe("convert RGB color to HEX color", () => {
-        it("should convert color rgb(100, 200, 255) to #64C8FF", () => {
-            assert.deepEqual(Color.toHex("100,200,255"), "#64C8FF");
-        });
+  describe("convert RGB color to HEX color", () => {
+    it("should convert color rgb(100,200,255) to #64C8FF", () => {
+      assert.deepEqual(Color.toHex("100,200,255"), "#64C8FF");
+    });
+  });
+
+  describe("convert HEX color to RGB color", () => {
+    it("should convert #F8E100 to rgb(248,225,0)", () => {
+      assert.deepEqual(Color.toRgb("#F8E100"), "248,225,0");
+    });
+    it("Should convert various Hex codes to RGB", () => {
+      assert.deepEqual(Color.toRgb('#F8E100'), '248,225,0');
+      assert.deepEqual(Color.toRgb('#FADE00', true), [250, 222, 0]);
+      assert.deepEqual(Color.toRgb('#000000'), '0,0,0');
+      assert.deepEqual(Color.toRgb('#010101'), '1,1,1');
+      assert.deepEqual(Color.toRgb('#101010'), '16,16,16');
+      assert.deepEqual(Color.toRgb('#111111'), '17,17,17');
+      assert.deepEqual(Color.toRgb('#000'), '0,0,0');
+      assert.deepEqual(Color.toRgb('#111'), '17,17,17');
+    });
+  });
+
+  describe("autodetect color type and return it", () => {
+    it("should detect hex types if this one is a valid color", () => {
+      assert.deepEqual(Color.getType("#000000"), "hex");
+      assert.deepEqual(Color.getType("#000"), "hex");
+      assert.deepEqual(Color.getType("#ABC"), "hex");
+      assert.deepEqual(Color.getType("#abc"), "hex");
+      assert.deepEqual(Color.getType("#aBc"), "hex");
+      assert.deepEqual(Color.getType("#A01"), "hex");
     });
 
-    describe("convert HEX color to RGB color", () => {
-        it("should convert #F8E100 to rgb(248, 225, 0)", () => {
-            assert.deepEqual(Color.toRGB("#F8E100"), "248, 225, 0");
-        });
-        it ("Should convert various Hex codes to RGB", () => {
-          assert.deepEqual(Color.toRgb('#F8E100'), '248,225,0');
-          assert.deepEqual(Color.toRgb('#FADE00', true), [250,222,0]);
-          assert.deepEqual(Color.toRgb('#000000'), '0,0,0');
-          assert.deepEqual(Color.toRgb('#010101'), '1,1,1');
-          assert.deepEqual(Color.toRgb('#101010'), '16,16,16');
-          assert.deepEqual(Color.toRgb('#111111'), '17,17,17');
-          assert.deepEqual(Color.toRgb('#000'), '0,0,0');
-          assert.deepEqual(Color.toRgb('#111'), '17,17,17');
-        });
+    it("should return undefined if the HEX color is invalid", () => {
+      assert.deepEqual(typeof Color.getType("000000"), "undefined");
+      assert.deepEqual(typeof Color.getType("#1234"), "undefined");
+      assert.deepEqual(typeof Color.getType("#12345"), "undefined");
+      assert.deepEqual(typeof Color.getType("#12345X"), "undefined");
     });
 
-    describe("autodetect color type and return it", () => {
-      it("should detect hex types if this one is a valid color", () => {
-        assert.deepEqual(Color.getType("#000000"), "hex");
-        assert.deepEqual(Color.getType("#000"), "hex");
-        assert.deepEqual(Color.getType("#ABC"), "hex");
-        assert.deepEqual(Color.getType("#abc"), "hex");
-        assert.deepEqual(Color.getType("#aBc"), "hex");
-        assert.deepEqual(Color.getType("#A01"), "hex");
-      });
-
-      it("should return undefined if the HEX color is invalid", () => {
-        assert.deepEqual(typeof Color.getType("000000"), "undefined");
-        assert.deepEqual(typeof Color.getType("#1234"), "undefined");
-        assert.deepEqual(typeof Color.getType("#12345"), "undefined");
-        assert.deepEqual(typeof Color.getType("#12345X"), "undefined");
-      });
-
-      it("should detect the RGB type and return it if this one is a valid color", () => {
-        assert.deepEqual(Color.getType("0, 0, 0"), "rgb");
-        assert.deepEqual(Color.getType("255, 255, 255"), "rgb");
-        assert.deepEqual(Color.getType("123, 123, 123"), "rgb");
-      });
-
-      it("should return undefined if the RGB color is invalid", () => {
-        assert.deepEqual(typeof Color.getType("0, 0, 256"), "undefined");
-        assert.deepEqual(typeof Color.getType("0, 0, 01"), "undefined");
-        assert.deepEqual(typeof Color.getType("255, 255, 0255"), "undefined");
-        assert.deepEqual(typeof Color.getType("10, 10, 1000"), "undefined");
-        assert.deepEqual(typeof Color.getType("1,1,1"), "undefined");
-        assert.deepEqual(typeof Color.getType(" 1, 1, 1"), "undefined");
-        assert.deepEqual(typeof Color.getType("1, 1, 1 "), "undefined");
-      });
+    it("should detect the RGB type and return it if this one is a valid color", () => {
+      assert.deepEqual(Color.getType("0, 0, 0"), "rgb");
+      assert.deepEqual(Color.getType("255, 255, 255"), "rgb");
+      assert.deepEqual(Color.getType("123, 123, 123"), "rgb");
     });
+
+    it("should return undefined if the RGB color is invalid", () => {
+      assert.deepEqual(typeof Color.getType("0, 0, 256"), "undefined");
+      assert.deepEqual(typeof Color.getType("0, 0, 01"), "undefined");
+      assert.deepEqual(typeof Color.getType("255, 255, 0255"), "undefined");
+      assert.deepEqual(typeof Color.getType("10, 10, 1000"), "undefined");
+      assert.deepEqual(typeof Color.getType("1,1,1"), "undefined");
+      assert.deepEqual(typeof Color.getType(" 1, 1, 1"), "undefined");
+      assert.deepEqual(typeof Color.getType("1, 1, 1 "), "undefined");
+    });
+  });
 });
